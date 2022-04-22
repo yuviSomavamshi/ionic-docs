6bf331d54b19df1ead29f4bc8267f397119e3fd2
Yuvaraj K <yuvarajsomavamshi@gmail.com>
2022-04-22 14:20:09 +0530

    cleanup

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,15 @@ module.exports = {
       type: 'category',
       label: 'Getting Started',
       collapsed: false,
-      items: ['index', 'intro/upgrading-to-ionic-6', 'intro/environment', 'intro/cli', 'intro/cdn', 'intro/vscode-extension', 'intro/next'],
+      items: [
+        'index',
+        'intro/upgrading-to-ionic-6',
+        'intro/environment',
+        'intro/cli',
+        'intro/cdn',
+        'intro/vscode-extension',
+        'intro/next',
+      ],
     },
     {
       type: 'category',
